628a4636ff636a13d5e44bc179883f60ccb96da3
Amanjha0008 <aman.jha@knoldus.com>
2024-04-07 11:13:11 +0530

    adding new code

--- src/test/java/helper/BrowserFactory.java
+++ src/test/java/helper/BrowserFactory.java
@@ -2,24 +2,34 @@ package helper;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BrowserFactory {
     public static WebDriver driver;
-    public static WebDriver startBrowser(String browserName, String url){
-        if(browserName.equals("chrome")){
-            driver = new ChromeDriver();
 
-        } else if (browserName.equalsIgnoreCase("firefox")) {
-            driver = new FirefoxDriver();
-
-        }
-        else if (browserName.equalsIgnoreCase("edge")){
-            driver = new EdgeDriver();
+    public static WebDriver startBrowser(String browserName, String url) {
+        switch (browserName.toLowerCase()) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--start-maximized");
+                driver = new ChromeDriver(chromeOptions);
+                break;
+            case "firefox":
+                // Firefox setup
+                break;
+            case "edge":
+                // Edge setup
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid browser name: " + browserName);
         }
         driver.manage().window().maximize();
         driver.get("https://www.amazon.in/");
         return  driver;
     }
 }
+
